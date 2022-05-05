baf08a7a38563e1fb250b5988c3fa16b723bae03
HeryKely <heryrakotomangahr@gmail.com>
2022-05-05 15:18:17 +0300

    mon premier commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+$(document.ready(function(){
+    $('#menu_un').click(function(){
+        $('#exemple').hide()
+        $('#fr1').hide()
+        $('#fr2').hide()
+        $('#fr3').show()
+     
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}))
